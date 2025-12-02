c5e77b69d0c2f285b1a91a00a4b9bde90cb2199e
Md Tohid Reza <harishalone786@gmail.com>
2025-12-02 20:22:59 +0545

    express JS

--- index.js
+++ index.js
@@ -1,61 +1,26 @@
 import express from 'express'
-import {  userLogin, userSignup } from './controller.js'
-import router from './route.js'
+import {storage} from './config/multer.js'
+
 
 
 const app = express()
+const upload= multer({storage,limits:{fileSize:1024*1024*5}})
 const PORT = 3000
 
+
+app.use(express.urlencoded({extended:true}))
+app.use(upload.single('image'))
+
 app.get('/',(req,res)=>{
-    res.send('Hello, Tohid from Express')
+    res.send('Welcome to Home Page')
 })
-
-app.use('/user',router)
-app.use('/user',express.json())
-
-
-app.get('/things/:name/:id',(req,res)=>{
-    const {name, id}= req.params;
-    res.json({
-        id,
-        name
-    })
+app.post('/form',(req,res)=>{
+    console.log(req.body);
+    console.log(req.files);
+    
+    res.send('Form Submitted')
+    
 })
-
-//catch-all invalid route
-app.use((req, res) => {
-  res.status(404).send("Sorry, This is an invalid route");
-});
-
-
-app.post('/user', (req,res)=>{
-    const {name, email} = req.body;
-    res.json({
-        message: `User ${name} with email ${email} created successfully`
-    })
-})
-
-app.put('/user/:id',(req,res)=>{
-    const userId=req.params.id;
-    const{name,email}=req.body;
-    res.json({
-        message:`User with ID ${userId} updated to ${name} and email is ${email}`
-    })
-})
-
-app.delete('/user/:id',(req,res)=>{
-    const userId=req.params.id;
-    res.json({
-        message:`User with ID ${userId} deleted successfully`
-    })
-})
-
-
-
-
-
-
-
 
 
 
