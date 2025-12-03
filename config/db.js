2b4aab54d1617791b6b96f376a7dbef3cd1d679a
Md Tohid Reza <harishalone786@gmail.com>
2025-12-03 20:32:34 +0545

    ExpressJS

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 export const connectDB = async () =>{
-const MONGODB_URI = 'mongodb+srv://tohidreza786_db:Tohid123@cluster0.lhm0g2j.mongodb.net/express'
+const MONGODB_URI = 'databas url'
  await mongoose.connect(MONGODB_URI)
 .then(()=>{
     console.log('Database is successfully connected');
--- index.js
+++ index.js
@@ -1,42 +1,12 @@
 import express from 'express'
-import { connectDB } from "./config/db.js";
-import { Person } from './models/Person.js';
+import cookieParser from 'cookie-parser'
+//import session from 'express-session'
 
 
 const app = express()
 const PORT = 3000
 
-await connectDB();
-app.use(express.json());
 
-app.get('/',(req,res)=>{
-    res.send("Hello Express")
-})
-
-// saving data to mongodb
-app.post('/person',express.json(),async(req,res)=>{
-    console.log(req.body);
-    const {name,age,email} = req.body;
-    const newPerson = new Person({
-        name,
-        age,
-        email
-    })
-    await newPerson.save();
-    console.log("Person data saved to database");
-    res.send("Person data received")
-})
-
-//updating data in mongodb
-app.put("/person",  async (req, res) => {
-  console.log(req.body);
-  const { name } = req.body;
-  const personData = await Person.find({name})
-  console.log(personData);
-  
-  
-  res.send("Person data received");
-});
 
 
 app.listen(PORT,(req,res)=>{
